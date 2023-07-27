b27bdea4a84c40ae8109b5afb9ae2a5730e6611f
arkaddev <arek1994mr@gmail.com>
2023-07-27 22:18:33 +0200

    added information about results

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,34 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Witaj w programie wordsApp 0.1");
 
-        List<String> sortedList = FileReader.sortList(FileReader.readFile("slowa.txt"));
+        System.out.println("-----------------");
+        System.out.println("1. Test");
+        System.out.println("2.");
+        System.out.println("3. Lista wynikow");
 
-        while (true) {
-            String randomWord = FileReader.randomWord(sortedList);
-            String[] meaning = Word.splitWord(randomWord);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wybierz menu:");
+        int menu = scanner.nextInt();
+        switch (menu) {
+            case 1:
+                List<String> sortedList = FileReader.sortList(FileReader.readFile("slowa.txt"));
 
-            System.out.println(Word.checkAnswer(meaning));
+                for (int i = 0; i < 10; i++) {
+                    String randomWord = FileReader.randomWord(sortedList);
+                    String[] meaning = Word.splitWord(randomWord);
 
+                    System.out.println(Word.checkAnswer(meaning));
+                }
+
+                System.out.println(Word.results());
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                System.out.println("Nieprawidlowy numer");
         }
 
 
--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Word {
 
+    static int[] goodBadAnswers = new int[2];
     public static String[] splitWord(String word) {
 
         String[] result = new String[2];
@@ -13,14 +14,23 @@ public class Word {
 
 
     public static String checkAnswer(String[] separatedWord) {
+
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj znaczenie slowa: " + separatedWord[0]);
         String input = scanner.nextLine();
 
         if (separatedWord[1].equals(input)) {
+            goodBadAnswers[0]++;
             return "ok";
         }
-        return "zla odpowiedz";
+        goodBadAnswers[1]++;
+        return "zle, poprawna odpowiedz to : " + separatedWord[1];
 
+    }
+
+    public static String results(){
+
+        return goodBadAnswers[0] + " - odpowiedzi poprawne\n" + goodBadAnswers[1] + " - odpowiedzi bledne";
     }
 }
