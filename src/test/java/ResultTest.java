e7295260abfe1dc44d6ee925f7747b4d0c981483
arkaddev <arek1994mr@gmail.com>
2023-08-06 17:38:56 +0200

    added sortObjectList test

--- src/test/java/ResultTest.java
+++ src/test/java/ResultTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,5 +22,16 @@ class ResultTest {
 
     @Test
     void sortObjectList() {
+        List<Result> resultList = new ArrayList<>();
+        resultList.add(new Result(20,"Jan","02-08-2023 19:34:18"));
+        resultList.add(new Result(10,"Jan","02-08-2023 19:34:18"));
+        resultList.add(new Result(30,"Jan","02-08-2023 19:34:18"));
+
+        Result result = new Result();
+        result.sortObjectList(resultList);
+
+        assertEquals(30, resultList.get(0).getOutcome());
+        assertEquals(20, resultList.get(1).getOutcome());
+        assertEquals(10, resultList.get(2).getOutcome());
     }
 }
